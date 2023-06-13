4dffddec8eda3329cb474f515495f2c44e32c4dc
PirminP <pirmin.pernsteiner@gmail.com>
2023-06-13 19:12:03 -0300

    feat task2: implement txt_importer function to import news from a .txt file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido')
+        return []
+    try:
+        with open(path_file, 'r') as file:
+            line = file.read().split('\n')
+        return line
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
