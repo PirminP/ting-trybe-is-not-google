ed28d630fd33f42ac0ef5a80263b817b01288759
PirminP <pirmin.pernsteiner@gmail.com>
2023-06-14 17:26:06 -0300

    feat task7: implement exists_word function to verify word existence in all processed arq

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for archive in instance._data:
+        info_file = {
+            'palavra': word,
+            'arquivo': archive['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+
+        for i in range(archive['qtd_linhas']):
+            if word.lower() in archive['linhas_do_arquivo'][i].lower():
+                info_file['ocorrencias'].append({'linha': i + 1})
+
+        if info_file['ocorrencias']:
+            result.append(info_file)
+
+    return result
 
 
 def search_by_word(word, instance):
